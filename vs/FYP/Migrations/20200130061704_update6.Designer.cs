f3645acdc68eeb176e9a897388da08c2dea80000
Pluggins <garytan@amazecraft.net>
2020-01-30 15:01:57 +0800

    Migration

--- vs/FYP/Migrations/20200130061704_update6.Designer.cs
+++ vs/FYP/Migrations/20200130061704_update6.Designer.cs
@@ -4,14 +4,16 @@ using FYP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FYP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200130061704_update6")]
+    partial class update6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
